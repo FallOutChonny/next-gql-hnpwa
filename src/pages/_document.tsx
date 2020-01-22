dd382d79e907132c03131b3e397701634d88cd7d
Chonny <mama.whowho@gmail.com>
2020-01-22 11:14:51 +0800

    chore: setting up with styled-components

---
+++ src/pages/_document.tsx
@@ -0,0 +1,29 @@
+import _Document, { DocumentContext } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class Document extends _Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await _Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+}
